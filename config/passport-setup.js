e9aa8774147c8680fba34754638f309ddc71e3b7
Abdul Rehman <mehars.6925@gmail.com>
2018-03-02 22:19:23 +0500

    complete login system with passport-js

---
+++ config/passport-setup.js
@@ -0,0 +1,117 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const FacebookStrategy = require('passport-facebook');
+const TwitterStrategy = require('passport-twitter');
+const keys = require('./keys');
+const User = require('../models/user-model');
+
+let getImg4mGp = (urI) => { // Get Full Size Image From G+
+  return urI.replace(/sz=50/, '');
+};
+
+let getImg4mTwitter = (urI) => { // Get Full Size Image From G+
+  return urI.replace(/_normal/, '');
+};
+
+/*
+---------- serialize & deserialize process
+*/
+
+passport.serializeUser((user,done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+/*
+---------- Twitter Strategy
+*/
+
+passport.use(new TwitterStrategy({
+  consumerKey: keys.twitter.consumerKey,
+  consumerSecret: keys.twitter.consumerSecret,
+  callbackURL: keys.twitter.callbackURL
+},(token, tokenSecret, profile, done) => {
+    User.findOne({ provider: profile.provider, userId: profile.id }).then((userExsists) => {
+      if (userExsists) {
+        done(null, userExsists);
+      }else{
+        new User({
+          userId: profile.id,
+          name: profile.displayName,
+          gender: "undefined",
+          photo: getImg4mTwitter(profile.photos[0].value),
+          provider: profile.provider,
+          status: "online"
+        }).save().then((newUser) => {
+          done(null, newUser);
+        });
+      }
+    });
+  }
+));
+
+
+/*
+---------- Facebook Strategy
+*/
+
+passport.use(new FacebookStrategy({
+  clientID: keys.facebook.clientID,
+  clientSecret: keys.facebook.clientSecret,
+  callbackURL: keys.facebook.callbackURL
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({provider: profile.provider, userId: profile.id}).then((userExsists) => {
+    if(userExsists){
+      done(null, userExsists);
+    }else{
+      let gender , photo;
+      (profile.gender) ? gender = profile.gender : gender = "undefined";
+      (profile.photos[0].value) ? photo = profile.photos[0].value : photo = "../img/person/dummy.jpg";
+      new User({
+        userId: profile.id,
+        name: profile.displayName,
+        gender: gender,
+        photo: photo,
+        provider: profile.provider,
+        status: "online"
+      }).save().then((newUser) => {
+        done(null, newUser);
+      });
+    }
+  });
+}
+));
+
+/*
+---------- Google plus Strategy
+*/
+
+passport.use(new GoogleStrategy({
+  clientID: keys.google.clientID,
+  clientSecret: keys.google.clientSecret,
+  callbackURL: keys.google.callbackURL
+}, (accessToken,refreshToken,profile,done) => {
+  User.findOne({provider: profile.provider, userId: profile.id}).then((userExsists) => {
+    if(userExsists){
+      done(null, userExsists);
+    }else{
+      new User({
+        userId: profile.id,
+        name: profile.displayName,
+        gender: profile.gender,
+        photo: getImg4mGp(profile.photos[0].value),
+        provider: profile.provider,
+        status: "online"
+      }).save().then((newUser) => {
+        done(null, newUser);
+      });
+    }
+  });
+}));
+
+
