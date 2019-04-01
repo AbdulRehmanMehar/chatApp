3a79b666f2b6a663b4f48a241b177d12cab5a67e
Abdul Rehman <mehars.6925@gmail.com>
2019-04-01 21:52:03 +0500

    updated

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,4 +1,3 @@
-const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const FacebookStrategy = require('passport-facebook');
 const TwitterStrategy = require('passport-twitter');
@@ -13,33 +12,36 @@ let getImg4mTwitter = (urI) => { // Get Full Size Image From G+
   return urI.replace(/_normal/, '');
 };
 
-/*
----------- serialize & deserialize process
-*/
 
-passport.serializeUser((user,done) => {
-  done(null, user.id);
-});
+module.exports = (passport) => {
 
-passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
+  /*
+  ---------- serialize & deserialize process
+  */
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
   });
-});
 
-/*
----------- Twitter Strategy
-*/
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+      done(null, user);
+    });
+  });
 
-passport.use(new TwitterStrategy({
-  consumerKey: keys.twitter.consumerKey,
-  consumerSecret: keys.twitter.consumerSecret,
-  callbackURL: keys.twitter.callbackURL
-},(token, tokenSecret, profile, done) => {
+  /*
+  ---------- Twitter Strategy
+  */
+
+  passport.use(new TwitterStrategy({
+    consumerKey: process.env.twitterKey || keys.twitter.consumerKey,
+    consumerSecret: process.env.twitterSecret || keys.twitter.consumerSecret,
+    callbackURL: process.env.twitterCb || keys.twitter.callbackURL
+  }, (token, tokenSecret, profile, done) => {
     User.findOne({ provider: profile.provider, userId: profile.id }).then((userExsists) => {
       if (userExsists) {
         done(null, userExsists);
-      }else{
+      } else {
         new User({
           userId: profile.id,
           name: profile.displayName,
@@ -53,65 +55,65 @@ passport.use(new TwitterStrategy({
       }
     });
   }
-));
+  ));
 
 
-/*
----------- Facebook Strategy
-*/
+  /*
+  ---------- Facebook Strategy
+  */
 
-passport.use(new FacebookStrategy({
-  clientID: keys.facebook.clientID,
-  clientSecret: keys.facebook.clientSecret,
-  callbackURL: keys.facebook.callbackURL
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({provider: profile.provider, userId: profile.id}).then((userExsists) => {
-    if(userExsists){
-      done(null, userExsists);
-    }else{
-      let gender , photo;
-      (profile.gender) ? gender = profile.gender : gender = "undefined";
-      (profile.photos[0].value) ? photo = profile.photos[0].value : photo = "../img/person/dummy.jpg";
-      new User({
-        userId: profile.id,
-        name: profile.displayName,
-        gender: gender,
-        photo: photo,
-        provider: profile.provider,
-        status: "online"
-      }).save().then((newUser) => {
-        done(null, newUser);
-      });
-    }
-  });
+  passport.use(new FacebookStrategy({
+    clientID: process.env.facebookKey || keys.facebook.clientID,
+    clientSecret: process.env.facebookSecret || keys.facebook.clientSecret,
+    callbackURL: process.env.facebookCb || keys.facebook.callbackURL
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ provider: profile.provider, userId: profile.id }).then((userExsists) => {
+      if (userExsists) {
+        done(null, userExsists);
+      } else {
+        let gender, photo;
+        (profile.gender) ? gender = profile.gender : gender = "undefined";
+        (profile.photos[0].value) ? photo = profile.photos[0].value : photo = "../img/person/dummy.jpg";
+        new User({
+          userId: profile.id,
+          name: profile.displayName,
+          gender: gender,
+          photo: photo,
+          provider: profile.provider,
+          status: "online"
+        }).save().then((newUser) => {
+          done(null, newUser);
+        });
+      }
+    });
+  }
+  ));
+
+  /*
+  ---------- Google plus Strategy
+  */
+
+  passport.use(new GoogleStrategy({
+    clientID:  process.env.googleKey || keys.google.clientID,
+    clientSecret: process.env.googleSecret ||  keys.google.clientSecret,
+    callbackURL: process.env.googleCb ||  keys.google.callbackURL
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ provider: profile.provider, userId: profile.id }).then((userExsists) => {
+      if (userExsists) {
+        done(null, userExsists);
+      } else {
+        new User({
+          userId: profile.id,
+          name: profile.displayName,
+          gender: profile.gender,
+          photo: getImg4mGp(profile.photos[0].value),
+          provider: profile.provider,
+          status: "online"
+        }).save().then((newUser) => {
+          done(null, newUser);
+        });
+      }
+    });
+  }));
+
 }
-));
-
-/*
----------- Google plus Strategy
-*/
-
-passport.use(new GoogleStrategy({
-  clientID: keys.google.clientID,
-  clientSecret: keys.google.clientSecret,
-  callbackURL: keys.google.callbackURL
-}, (accessToken,refreshToken,profile,done) => {
-  User.findOne({provider: profile.provider, userId: profile.id}).then((userExsists) => {
-    if(userExsists){
-      done(null, userExsists);
-    }else{
-      new User({
-        userId: profile.id,
-        name: profile.displayName,
-        gender: profile.gender,
-        photo: getImg4mGp(profile.photos[0].value),
-        provider: profile.provider,
-        status: "online"
-      }).save().then((newUser) => {
-        done(null, newUser);
-      });
-    }
-  });
-}));
-
-
