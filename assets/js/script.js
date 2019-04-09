20b48c2b9f7ec85a7dbbf24d13d0c2de8f4882ae
Abdul Rehman <mehars.6925@gmail.com>
2019-04-09 23:40:38 +0500

    handles chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,67 @@
 
+let userElement = document.querySelector('#users');
 
+let messageElement = document.querySelector('#messages');
+let cuser = document.querySelector('meta[name="user"]').attributes['content'].value;
 
-/*
-------- Socket.io
-*/
-let socket = io.connect('http://localhost:3000');
-
-
-// Query DOM
-let message = document.getElementById("message-to-send"),
-    btn = document.getElementById("send"),
-    handle = document.getElementById("handle"),
-    output = document.getElementById("output"),
-    userID = document.getElementById("userID"),
-    feedback = document.getElementById("feedback"),
-    clientID = document.getElementById('clientID');
-// Emmit Events
-btn.addEventListener('click' , () => {
-  socket.emit('chat',{
-    message: message.value,
-    handle: handle.value,
-    userID: userID.value,
-    clientID: clientID.value
+let socket = io.connect(window.location.host);
+socket.on('connect', () => {
+  socket.on('users', users => {
+    users.map(user => {
+      if(user.uid != cuser){
+        let name = user.user.name;
+        let photo = user.user.photo;
+        let child = document.getElementById(user.uid);
+        if(!userElement.contains(child)) {
+            userElement.innerHTML += `
+              <div id="${user.uid}" uphoto="${photo}" uname="${name}" class="mt-1 mb-1 user" onclick="setReciever(this)">
+                <div class="bg" style="background-image: url(${photo}); float: left; margin-right: 10px;"></div>
+                <div>
+                  <span>${name}</span>
+                </div>
+              </div>
+            `;
+        }
+      }
+    })
   });
-  message.value = "";
-});
 
-message.addEventListener('keypress', () => {
-  socket.emit('typing',handle.value);
-});
-
-// Listen for Events
-socket.on('chat', (data) => {
-    feedback.innerHTML = "";
-    if(data.userID == userID.value){
-      output.innerHTML += '<li class="clearfix" id="last">' +
-        '<div class="message-data ">' + 
-          '<i class="fa fa-circle online"></i> <span class="message-data-name" >'+ data.handle +'</span>' +
-        '</div>' +
-        '<div class="message my-message">' + data.message + '</div>'+
-      '</li>';
-    }else{
-      output.innerHTML += '<li class="clearfix" id="last">' +
-        '<div class="message-data align-right">' + 
-          '<span class="message-data-name" >'+ data.handle +'</span> <i class="fa fa-circle other"></i> ' +
-        '</div>' +
-        '<div class="message other-message float-right">' + data.message + '</div>'+
-      '</li>';
-    }
-    document.getElementById('link').click();
+  socket.on('message', (data) => {
+    messageElement.innerHTML += ` 
+      <div class="message recieved"><span class="data">${data.message}<span></div> 
+    `;
+    messageElement.scrollTop = messageElement.scrollHeight - messageElement.clientHeight;
+  });
 
 });
 
-socket.on('typing', (data) => {
-  feedback.innerHTML = '<i class="fa fa-circle online"></i>' +
-  '<i class="fa fa-circle online" style="color: #AED2A6"></i> ' +
-  '<i class="fa fa-circle online" style="color:#DAE9DA"></i> &nbsp;' + data + ' is typing...';
-});
 
+let setReciever = (event) => {
+  let ref = document.getElementById(event.id);
+  let reciever = document.querySelector('#message-to');
+  reciever.value = event.id;
+  userElement.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
+  document.getElementById(event.id).classList.add('active');
+  document.getElementById('current-user-name').innerHTML = ref.attributes.uname.value;
+  document.getElementById('current-user-bg').style.backgroundImage = `url(${ref.attributes.uphoto.value})`;
+}
+
+let sendMessage = (event) => {
+  let msg = document.querySelector('#text-message');
+  let reciever = document.querySelector('#message-to');
+  let data = {
+    to: reciever.value,
+    message: msg.value,
+  };
+  if(!data.to || !data.message){
+    event.preventDefault();
+    return alert('Please select user and write message.');
+  }
+  socket.emit('message', data);
+  messageElement.innerHTML += `
+    <div class="message"><span class="data">${data.message}</span></div> 
+  `;
+  messageElement.scrollTop = messageElement.scrollHeight - messageElement.clientHeight;
+  msg.value = '';
+  event.preventDefault();
+}
