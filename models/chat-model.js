c8f245ff7f0d779373680a88ed596df50d3c22d6
Abdul Rehman <mehars.6925@gmail.com>
2019-04-10 12:30:18 +0500

    updated

--- models/chat-model.js
+++ models/chat-model.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const chatSchema = new Schema({
-  users: {
-    userOneID: String,
-    userTwoID: String
-  },
-  chatData: [
-    {
-      message: String,
-      userID: String
-    }
-  ]
+  to: mongoose.Schema.Types.ObjectId,
+  from: mongoose.Schema.Types.ObjectId,
+  message: String,
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Chat = mongoose.model('chat',chatSchema);
+const Chat = mongoose.model('chat', chatSchema);
 
 module.exports = Chat;
